453b97a686fa72704077b27482d026faeb8709f0
fireblade <fireblade211@gmail.com>
2014-10-13 15:47:26 -0400

    Init

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	private static Main client;
+
+	public Main() {
+		System.out.println("sucess");
+	}
+
+	public static void main(String[] args) {
+		client = new Main();
+	}
+	
+	public void init() {
+		client = new Main();
+	}
+	
+	
+
+}
